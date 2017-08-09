cb71da9888043399aafa6c3630e65a13d0d95450
a632079 <a632079@qq.com>
2017-08-09 18:08:05 +0800

    Update to 0.3.0

--- library.js
+++ library.js
@@ -1,16 +1,12 @@
 "use strict";
 const meta = module.parent.require('./meta');
-let core = {};
-const constants = Object.freeze({
-	'name': "aliverify",
-	'admin': {
-		'icon': 'fa-tasks',
-		'route': '/plugins/aliverify'
+let core = {}, ALY = require("aliyun-sdk"), app_key, app_secret;
+meta.settings.get('aliverify', function (err, settings) {
+	if (!err && settings['key'] && settings['secret']) {
+		app_key = settings['key'];
+		app_secret = settings['secret'];
 	}
 });
-const ALY = require("aliyun-sdk");
-const app_key = meta.config['aliverify:key'];
-const app_secret = meta.config['aliverify:secret'];
 //hook static:app.load
 core.init = (data, callback) => {
 	function renderAdmin(req, res) {
@@ -18,22 +14,35 @@ core.init = (data, callback) => {
 	}
 	data.router.get('/admin/plugins/aliverify', data.middleware.admin.buildHeader, renderAdmin);
 	data.router.get('/api/admin/plugins/aliverify', renderAdmin);
+	data.router.get('/aliverify/js/register', (req, res, next) => {
+		res.set('Content-Type', 'application/x-javascript');
+		res.charset = 'utf-8';
+		res.sendfile("./node_modules/nodebb-plugin-registration-aliverify/static/lib/register.js");
+	});
+	data.router.get('/aliverify/js/login', (req, res, next) => {
+		res.set('Content-Type', 'application/x-javascript');
+		res.charset = 'utf-8';
+		res.sendfile("./node_modules/nodebb-plugin-registration-aliverify/static/lib/login.js");
+	})
 	callback();
 };
 //hook filter:admin.header.build
 core.addAdminNavigation = (custom_header, callback) => {
 	custom_header.plugins.push({
-		"route": constants.admin.route,
-		"icon": constants.admin.icon,
+		"icon": "fa-tasks",
+		"route": "/plugins/aliverify",
 		"name": "阿里云验证服务"
 	});
 
 	callback(null, custom_header);
 };
 //hook filter:register.build
-core.addverify = (data, callback) => {
-	console.log(`hook filter:register.build`);
-	console.log(data);
+core.regcaptcha = (data, callback) => {
+	console.log("hook filter:register.build");
+	//console.log(data);
+	if (!app_key || !app_secret) {
+		callback(null, data);
+	}
 	let ret = {
 		"label": "验证码",
 		"html": '<div class="well"><div id="nc_captcha"></div></div>'
@@ -54,10 +63,12 @@ core.addverify = (data, callback) => {
 	callback(null, data);
 };
 //hook filter:register.check
-core.check = (data, callback) => {
-	console.log(`hook filter:register.check`);
+core.regcheck = (data, callback) => {
+	console.log("hook filter:register.check");
+	if (!app_key || !app_secret) {
+		callback(null, data);
+	}
 	console.log(data);
-
 	let jaq = new ALY.JAQ({
 		accessKeyId: app_key,
 		secretAccessKey: app_secret,
@@ -77,7 +88,74 @@ core.check = (data, callback) => {
 		if (err) {
 			//异常
 			console.log('error:', err);
-			callback(err,data)
+			callback(err, data)
+			return;
+		}
+		//此处无异常，但也可能调用失败
+		console.log('result:', JSON.stringify(d));
+	});
+
+	callback(null, data);
+};
+//hook filter:login.build
+core.logcaptcha = (data, callback) => {
+	console.log("hook filter:login.build");
+	//console.log(data);
+	if (!process.env.ali_login) {
+		callback(null, data);
+	}
+	if (!app_key || !app_secret) {
+		callback(null, data);
+	}
+	let ret = {
+		"label": "验证码",
+		"html": '<div class="well"><div id="nc_captcha"></div></div>'
+	};
+	let ali_key = app_key;
+	let nc_captcha = {
+		"html": "<p id='nc_appkey' class='hidden'>" + ali_key + "</p><input type='hidden' id='csessionid' name='csessionid'/><input type='hidden' id='sig' name='sig'/><input type='hidden' id='token' name='token'/><input type='hidden' id='scene' name='scene'/>"
+	};
+
+
+	if (data.templateData.regFormEntry && Array.isArray(data.templateData.regFormEntry)) {
+		data.templateData.regFormEntry.push(ret);
+		data.templateData.regFormEntry.push(nc_captcha);
+	} else {
+		data.templateData.captcha = ret;
+		data.templateData.nc_captcha = nc_captcha;
+	}
+	callback(null, data);
+};
+//hook filter:login.check
+core.logcheck = (data, callback) => {
+	console.log("hook filter:login.check");
+	if (!process.env.ali_login) {
+		callback(null, data);
+	}
+	if (!app_key || !app_secret) {
+		callback(null, data);
+	}
+	console.log(data);
+	let jaq = new ALY.JAQ({
+		accessKeyId: app_key,
+		secretAccessKey: app_secret,
+		endpoint: 'http://jaq.aliyuncs.com',
+		apiVersion: '2016-11-23',
+	});
+
+	jaq.afsCheck({
+
+		Platform: 3,//必填参数，请求来源： 1：Android端； 2：iOS端； 3：PC端及其他
+		Session: data.req.body['csessionid'],// 必填参数，从前端获取，不可更改
+		Sig: data.req.body['sig'],// 必填参数，从前端获取，不可更改
+		Token: data.req.body['token'],// 必填参数，从前端获取，不可更改
+		Scene: "login"// 必填参数，从前端获取，不可更改
+
+	}, function (err, d) {
+		if (err) {
+			//异常
+			console.log('error:', err);
+			callback(err, data)
 			return;
 		}
 		//此处无异常，但也可能调用失败
--- static/lib/core.js
+++ static/lib/core.js
@@ -1,14 +1,38 @@
+"use strict";
 //Check Windows Url
-$(()=>{
-    if(document.URL.match("/register")){
-        let ts = Date.now().toString();
+$(document).ready(function () {
+    if (Array.isArray(document.URL.match("/register"))) {
+        var ts = Date.now().toString();
         //Add Aliyun Check Head
         $("head").append("<!-- 此段必须要引入 t为小时级别的时间戳 -->");
-        $("head").append('<link type="text/css" href="//g.alicdn.com/sd/ncpc/nc.css?t='+ts+'" rel="stylesheet"/>');
-        $("head").append('<script type="text/javascript" src="//g.alicdn.com/sd/ncpc/nc.js?t='+ts+'"></script>');
+        $("head").append('<link type="text/css" href="//g.alicdn.com/sd/ncpc/nc.css?t=' + ts + '" rel="stylesheet"/>');
+        $.getScript("//g.alicdn.com/sd/ncpc/nc.js?t='+ts+'", function () {
+            console.log("阿里云验证码: nc.js loaded.");
+            $.getScript("./aliverify/js/register", function () {
+                console.log("阿里云验证码: ali.js loaded.");
+            });
+        });
+        $("head").append('<script type="text/javascript" src="//g.alicdn.com/sd/ncpc/nc.js?t=' + ts + '"></script>');
         $("head").append("<!-- 引入结束 丧心病狂的NodeBB，他要我通过js加 = = qnmd -->");
         //Add Check Body
         $("body").prepend('<div id="_umfp" style="display:inline;width:1px;height:1px;overflow:hidden"></div>');
-        $("body").append(`<script>var nc = new noCaptcha();var nc_appkey = $("#nc_appkey").text();var nc_scene = 'register';var nc_token = [nc_appkey, (new Date()).getTime(), Math.random()].join(':');var nc_option = {renderTo: '#nc_captcha',appkey: nc_appkey,scene: nc_scene,token: nc_token,callback: function (data) {console.log(data.csessionid);console.log(data.sig);console.log(nc_token);document.getElementById('csessionid').value = data.csessionid;document.getElementById('sig').value = data.sig;document.getElementById('token').value = nc_token;document.getElementById('scene').value = nc_scene;}};nc.init(nc_option);</script>`);
+        console.log('nodebb-plugin-aliverify: loaded;');
+    } else if (Array.isArray(document.URL.match("/login"))) {
+        var ts = Date.now().toString();
+        //Add Aliyun Check Head
+        $("head").append("<!-- 此段必须要引入 t为小时级别的时间戳 -->");
+        $("head").append('<link type="text/css" href="//g.alicdn.com/sd/ncpc/nc.css?t=' + ts + '" rel="stylesheet"/>');
+        $.getScript("//g.alicdn.com/sd/ncpc/nc.js?t='+ts+'", function () {
+            console.log("阿里云验证码: nc.js loaded.");
+            $.getScript("./aliverify/js/login", function () {
+                console.log("阿里云验证码: ali.js loaded.");
+            });
+        });
+        $("head").append('<script type="text/javascript" src="//g.alicdn.com/sd/ncpc/nc.js?t=' + ts + '"></script>');
+        $("head").append("<!-- 引入结束 丧心病狂的NodeBB，他要我通过js加 = = qnmd -->");
+        //Add Check Body
+        $("body").prepend('<div id="_umfp" style="display:inline;width:1px;height:1px;overflow:hidden"></div>');
+        console.log('nodebb-plugin-aliverify: loaded;');
     }
 });
+
