e20673db2d14ab2d0ae94dc8d87267c65d16e9ce
a632079 <a632079@qq.com>
2017-08-09 21:09:12 +0800

    Fix bugs.(CBack to ES5)

--- static/lib/core.js
+++ static/lib/core.js
@@ -3,24 +3,23 @@
 $(document).ready(function () {
     $(window).on('action:ajaxify.end', function (event, data) {
         if (data.url == "register") {
-            let ts = Date.now().toString();
+            var ts = Date.now().toString();
             $("head").append('<link type="text/css" href="//g.alicdn.com/sd/ncpc/nc.css?t=' + ts + '" rel="stylesheet"/>');
             //Add Check Body
             $("body").prepend('<div id="_umfp" style="display:inline;width:1px;height:1px;overflow:hidden"></div>');
-            let ctaaa = Date.now();
+            var ct = Date.now();
             console.log('nodebb-plugin-aliverify: loaded;');
             console.log("阿里云验证码: nc.js  Start loading");
             $.getScript("//g.alicdn.com/sd/ncpc/nc.js?t=" + ts, function () {
-                let ctas = Date.now() - ctaaa;
-                console.log("阿里云验证码: nc.js loaded. 耗时:" + ctas + "ms");
+                var cts = Date.now() - ct;
+                console.log("阿里云验证码: nc.js loaded. 耗时:" + cts + "ms");
                 ctaaa = Date.now();
                 console.log("阿里云验证码: ali.js  Start loading");
                 $.getScript("./aliverify/js/register", function (err) {
-                    ctas = Date.now() - ctaaa;
-                    console.log("阿里云验证码: ali.js loaded. 耗时:" + ctas + "ms");
+                    cts = Date.now() - ct;
+                    console.log("阿里云验证码: ali.js loaded. 耗时:" + cts + "ms");
                 });
             });
-            
         }
     });
 });
